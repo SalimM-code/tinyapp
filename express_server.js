1d6b70939549419ec79a152e0c24838596cff76e
salimM-code <mustafasalim000@gmail.com>
2021-09-27 20:19:37 -0400

    addded a function to generate shortURL

--- express_server.js
+++ express_server.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const app = express();
 const PORT = 3000; // default port 8080
-
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+function generateRandomString() {
+  let result = ' ';
+  let characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  let charactersLength = characters.length;
+  for (let i = 0; i < 6; i ++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -17,6 +29,10 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars)
 })
+//route to render POST req
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new")
+})
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}
@@ -29,6 +45,11 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+})
+//route to handle POST req
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
 })
 
 app.listen(PORT, () => {
